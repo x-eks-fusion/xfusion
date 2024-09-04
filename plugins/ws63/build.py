f32974211987744100f2a9a699d9518f2e7aef3f
dotdotchan <101727486+dotdotchan@users.noreply.github.com>
2024-09-04 18:41:55 +0800

    🦄 refactor(plugin_ws63): 新增固件拷贝；新增目标查询与指定 (#5)
    * 🦄 refactor(plugin_ws63): 增加编译与菜单命令下指定或查询目标的功能
    
    * 🦄 refactor(plugin_ws63): 增加编译后拷贝固件目录到工程目录下

--- plugins/ws63/build.py
+++ plugins/ws63/build.py
@@ -3,25 +3,52 @@ from xf_build import api
 import logging
 import os
 import shutil
+import glob
+import sys
 from pathlib import Path
 
+# 默认目标
+DEFAULT_TARGET = "ws63-liteos-app"
+# SDK 路径
+SDK_PATH: Path = api.XF_ROOT / "sdks/fbb_ws63/src"
+# SDK 编译输出的路径
+SDK_OUTPUT_PATH: Path = SDK_PATH / "output"
+XF_PROJECT_BUILD_PATH: Path = api.PROJECT_BUILD_PATH
 
 hookimpl = xf_build.get_hookimpl()
-SDK_PATH: Path = api.XF_ROOT / "sdks/fbb_ws63/src"; 
 class ws63():
     @hookimpl
     def build(self, args):
-        project_cmake_file: Path = api.PROJECT_BUILD_PATH / "build_environ.cmake"
+        project_cmake_file: Path = XF_PROJECT_BUILD_PATH / "build_environ.cmake"
         api.apply_template("cmake_project.j2", project_cmake_file)
         os.chdir(SDK_PATH.resolve())
-        api.exec_cmd(["python", "build.py", "-c -nhso -release ws63-liteos-app"])
-        return
-                
+        
+        target = DEFAULT_TARGET
+        # 第一个拓展参数为 help 时 显示 SDK 侧 编译时的可选目标
+        """
+        第一个拓展参数为（如果存在）：
+            help : 显示 SDK 侧 编译时的可选目标
+            其他 : 用户指定编译目标
+        """
+        if len(args) != 0:
+            if args[0] == "help":
+                target = ""
+            else:
+                target = args[0]
+        api.exec_cmd(["python", "build.py", "-c -nhso -release", target])
+
+        # 尝试将 output 下 fwpkg 目录复制到工程目录下的 build 目录下的 sdk 目录（如无则创建）
+        str_path_sdk_fw_list = glob.glob(f"{SDK_OUTPUT_PATH}/*/fwpkg") 
+        for str_path_fw in str_path_sdk_fw_list:
+            try:
+                shutil.copytree(str_path_fw, XF_PROJECT_BUILD_PATH / "sdk/fwpkg")
+            except:
+                print(f"copy sdk firmware failed!", sys.exc_info())
+          
     @hookimpl
     def clean(self, args):
-        ws63_output_path: Path = SDK_PATH / "output"
-        if ws63_output_path.exists() == True:
-            shutil.rmtree(ws63_output_path)
+        if SDK_OUTPUT_PATH.exists() == True:
+            shutil.rmtree(SDK_OUTPUT_PATH)
 
     @hookimpl
     def flash(self, args):
@@ -37,6 +64,19 @@ class ws63():
 
     @hookimpl
     def menuconfig(self, args):
+        # 第一个拓展参数为 sub 时 打开SDK 侧 menuconfig
         if args[0] == "sub":
+            target = DEFAULT_TARGET
+            """
+            第二个拓展参数为（如果存在）：
+                help : 显示 SDK 侧 menuconfig 可选目标
+                其他 : 用户指定 menuconfig 目标
+            """
+            if len(args) == 2:
+                if args[1] == "help":
+                    target = ""
+                else:
+                    target = args[1]
             os.chdir(SDK_PATH)
-            os.system("python build.py ws63-liteos-app menuconfig")
+            str_menuconfig_sub = f"python build.py menuconfig ${target}"
+            os.system(str_menuconfig_sub)
