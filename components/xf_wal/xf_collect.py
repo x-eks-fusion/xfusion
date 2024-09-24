0dc9fb2acb73283bc315498762e72977e66af26e
catcatblue <catcatblue@qq.com>
2024-09-24 14:48:53 +0800

    ✨ feat(components-xf_wal): 新增xf_wifi

--- components/xf_wal/xf_collect.py
+++ components/xf_wal/xf_collect.py
@@ -4,16 +4,19 @@ srcs = [
     "*.c",
     "xf_ble/*.c",
     "xf_sle/*.c",
+    "xf_wifi/*.c",
 ]
 
 incs = [
     ".",
     "xf_ble",
     "xf_sle",
+    "xf_wifi",
 ]
 
 reqs = [
-    "xf_utils"
+    "xf_utils",
+    "xf_nal",
 ]
 
 xf_build.collect(srcs=srcs, inc_dirs=incs, requires=reqs)
