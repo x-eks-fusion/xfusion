bbc34870bb8f3f235d0ce8ccbacffb4d055b2d02
ccb5 <b94e@outlook.com>
2024-10-10 14:37:13 +0800

    🦄 refactor(examples-sys): 添加看门狗与重启用例 (#1)
    * 🔧 build(boards-nearlink): 设置默认osal实现为cmsis
    
    * 🦄 refactor(examples-sys): 添加看门狗与重启用例

--- examples/system/sys/main/xf_main.c
+++ examples/system/sys/main/xf_main.c
@@ -16,6 +16,7 @@
 /* ==================== [Defines] =========================================== */
 
 #define TAG "main"
+#define EXAMPLE_DELAY_LOOP_COUNT        20  /*!< 有些看门狗超时时间很长 */
 
 /* ==================== [Typedefs] ========================================== */
 
@@ -42,6 +43,23 @@ void xf_main(void)
 
     XF_LOGI(TAG, "tick:us_before:%llu\tus_after:%llu\tus_diff:%llu",
             us_before, us_after, us_after - us_before);
+
+    /* 确保开启了 watchdog */
+    xf_sys_watchdog_enable();
+    xf_sys_watchdog_kick();
+    XF_LOGI(TAG, "Will reboot in %d seconds.", EXAMPLE_DELAY_LOOP_COUNT);
+    for (int i = EXAMPLE_DELAY_LOOP_COUNT; i > 0; --i) {
+        XF_LOGI(TAG, "count: %d", i);
+        /*
+            测试 kick 看门狗是否有作用，
+            如果有作用，应当在 EXAMPLE_DELAY_LOOP_COUNT s 后
+            输出 `reboot!` 消息并重启。
+         */
+        xf_sys_watchdog_kick();
+        xf_delay_ms(1000);
+    }
+    XF_LOGI(TAG, "reboot!");
+    xf_sys_reboot();
 }
 
 /* ==================== [Static Functions] ================================== */
