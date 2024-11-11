083355340d88f25f64c4198e26cdcb1c837b9140
cangyu <cangyu@pthyidh.com>
2024-11-11 17:11:04 +0800

    ✨ feat: 新增cJSON

--- boards/simulator/sim_linux/xmake.lua
+++ boards/simulator/sim_linux/xmake.lua
@@ -4,11 +4,13 @@ includes(path.join(os.getenv("XF_ROOT"),
 target(xf_project_name)
     set_kind("binary")
     add_includedirs(".")
+    add_includedirs("json")
+    add_files("./json/*.c")
     add_files("./main/*.c")
     add_deps("port_xf")
 
     -- 对应生成 -Wl,--whole-archive -la -lb -Wl,--no-whole-archive 链接选项
     -- 对于不支持的平台和编译，会退化成 -la -lb
     add_linkgroups("port_xf", "xf_build_desc", {whole = true})
-    
+
 
